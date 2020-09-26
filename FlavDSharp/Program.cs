1f4d31497263b65e8f32873a98d74860d115cdae
Flavius <51680989+Flavius-The-Person@users.noreply.github.com>
2020-09-26 03:01:17 -0400

    working DSharp Discord Bot

---
+++ FlavDSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using DSharpPlus;
+using System;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace FlavDSharp
+{
+    partial class Program
+    {
+        static DiscordClient discord;
+        static void Main(String[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = Creds.token,
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if(e.Message.ChannelId.Equals(614206030372667396) & e.Message.Content.StartsWith("ping"))
+                {
+                    await e.Message.RespondAsync("pong!");
+                }
+                else if(e.Message.Content.Equals("RAWR"))
+                {
+                    await e.Message.RespondAsync("RAWR!!!");
+                }
+
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
